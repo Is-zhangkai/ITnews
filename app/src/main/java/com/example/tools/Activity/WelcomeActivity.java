6c5ab56c675e320e94a42bb82f2bf5209b3a5916
zhang'kai <2993258983@qq.com>
2021-03-11 13:41:03 +0800

    ui

---
+++ app/src/main/java/com/example/tools/Activity/WelcomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.tools.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tools.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_welcome);
+    }
+}
