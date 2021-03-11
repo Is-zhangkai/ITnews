da3f4942d974e12eaeff57040c7b5770054b3018
zhang'kai <2993258983@qq.com>
2021-03-11 16:42:56 +0800

    ui3

--- app/src/main/java/com/example/tools/Activity/LoginActivity.java
+++ app/src/main/java/com/example/tools/Activity/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.tools.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.tools.R;
 
@@ -12,5 +14,22 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login);
+
+
+
+
+
+
+
+
+
+        findViewById(R.id.login_register).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,RegisterActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
